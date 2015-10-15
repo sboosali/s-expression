2f435aca8755d910395bdfd884983db8999e4210
Sam Boosalis <samboosalis@gmail.com>
2015-10-15 01:36:13 -0700

    document evalSplatSexp

--- sources/Data/Sexp.hs
+++ sources/Data/Sexp.hs
@@ -18,7 +18,7 @@ a <http://en.wikipedia.org/wiki/Common_Lisp#The_function_namespace Lisp-2> S-exp
 * @f@ is the function namespace
 * @a@ is the atom namespace
 
-you could @type Lisp1 a = Sexp a a@, but:
+you could define @type Lisp1 a = Sexp a a@, but:
 
 * @f@ is ignored by 'Monad'ic methods like 'joinSexp'
 * you might want to make sure to eval the @f@ to some normal form, like when matching against it in 'evalSexp'
@@ -29,12 +29,24 @@ the 'List' case is just a specialized @'Sexp' Void@, but easier to work with tha
 * @Sexp (Maybe f) [Sexp f a]@ (where Nothing would represent 'List')
 * forcing each concrete @f@ to hold a unit case (which would represent 'List')
 
+examples: 
+
+>>> 'toList' (List [Atom "f",Atom "x",List [Atom "g",Atom "y"],Atom "z"])
+["f","x","g","y","z"]
+
+>>> :{
+let doubleSexp e = do
+                    x <- e
+                    listSexp [x,x]
+:} 
+>>> doubleSexp (List [Atom "f", Sexp () [Atom "a", Atom "b"]])
+List [List [Atom "f",Atom "f"],Sexp () [List [Atom "a",Atom "a"],List [Atom "b",Atom "b"]]]
 
 -}
 data Sexp f a
- = Atom a                       -- 
- | List   [Sexp f a]            -- 
- | Sexp f [Sexp f a]            -- 
+ = Atom a 
+ | List   [Sexp f a]
+ | Sexp f [Sexp f a]
  deriving (Show,Read,Eq,Ord,Functor,Foldable,Traversable,Data,Generic)
 
 -- | default instance via the 'Monad' subclass.
@@ -44,6 +56,8 @@ instance Applicative (Sexp f) where
 
 {- |
 
+definitions:
+
 @
  'return' = 'pureSexp'
  '(>>=)' = 'bindSexp'
@@ -52,42 +66,68 @@ instance Applicative (Sexp f) where
 proofs of laws:
 
 
-* TODO : @join . return = id@
+* left-inverse(1): @join . return = id@
 
-@
-joinSexp (pureSexp m)
-joinSexp (Atom m)
-m
-@
-
-
-* TODO : @join . fmap return = id@
-
-@
-joinSexp (fmap pureSexp m)
-TODO
-
-m
-@
+    @
+    join (return m)
+    joinSexp (pureSexp m)
+    joinSexp (Atom m)
+    m
+    @
 
 
+* left-inverse(2): @join . fmap return = id@
+
+    (the Sexp case is elided, the steps being identical to the List case)
+
+    @
+    join (fmap return m)
+    joinSexp (fmap pureSexp m)
+    joinSexp (fmap Atom m)
+    -- case analysis 
+    case m of
+     Atom x ->
+      joinSexp (Atom (Atom x)) 
+      -- by definition of joinSexp
+      Atom x
+     List ms ->
+      joinSexp (List (fmap (fmap Atom) ms)
+      -- by definition of joinSexp
+      List (fmap joinSexp (fmap (fmap Atom) ms))
+      -- functor composition 
+      List (fmap (joinSexp . fmap Atom) ms)
+      List (fmap (join . fmap return) ms)
+      -- by induction
+      List (fmap id ms)
+      -- functor identity 
+      List ms
+     -- both cases are identity 
+    m
+    @
+    
+    where: 
+    
+    @
+    fmap f = \case
+     Atom x  -> f x 
+     List ms -> List (fmap (fmap f) ms)
+    
+    join = \case
+     Atom x  -> x 
+     List ms -> List (fmap joinSexp ms)
+    @
+    
 * associativity: @join . join = join . fmap join@
 
-@
-TODO
-@
-
-
-
-laws, verified as @QuickCheck@ properties:
-
-TODO
-
+    @
+    TODO
+    @
 
 -}
 instance Monad (Sexp f) where
  return = pure
  (>>=)  = bindSexp
+-- TODO laws, verified as @QuickCheck@ properties:
 
 instance Semigroup (Sexp f a) where
  (<>) = appendSexp
@@ -106,7 +146,7 @@ instance Plated (Sexp f a) where
 
 >>> :set -XOverloadedLists 
 >>> ["f", "x", ["g", "y"], "z"] :: Sexp f String
-
+List [Atom "f",Atom "x",List [Atom "g",Atom "y"],Atom "z"]
 
 -}
 instance IsList (Sexp f a) where
@@ -118,7 +158,7 @@ instance IsList (Sexp f a) where
 
 >>> :set -XOverloadedStrings  
 >>> "x" :: Sexp f String
-'Atom' "x" 
+Atom "x" 
 
 -}
 instance (IsString a) => IsString (Sexp f a) where
@@ -139,23 +179,23 @@ bindSexp s f = (joinSexp . fmap f) s
 -}
 joinSexp :: Sexp f (Sexp f a) -> Sexp f a
 joinSexp = \case
- Atom     s -> s
- List   sss -> List   (joinSexp <$> sss)
- Sexp f sss -> Sexp f (joinSexp <$> sss)
+ Atom     e -> e
+ List   ess -> List   (joinSexp <$> ess)
+ Sexp f ess -> Sexp f (joinSexp <$> ess)
 {-# INLINEABLE joinSexp #-} -- to hit the Atom I hope.
 
-{-| refines any Sexp to a list, which can be given to the List variant. 
+{-| refines any Sexp to a list, which can be given to the 'List'.
 -}
 toSexpList :: Sexp f a -> [Sexp f a]
 toSexpList = \case
- List ss -> ss
- s       -> [s]
+ List es -> es
+ e       -> [e]
 {-# INLINE toSexpList #-}
-
 
 {-| 
 
->>> appendSexp 
+>>> appendSexp (Atom "f") (List [Atom "x"])
+List [Atom "f",Atom "x"]
 
 -}
 appendSexp :: Sexp f a -> Sexp f a -> Sexp f a
@@ -168,21 +208,50 @@ emptySexp :: Sexp f a
 emptySexp = List []
 {-# INLINE emptySexp #-}
 
-{-| strictly evaluate a sexp ("all the way") to an atom.
+{-| fold over an sexp. 
 
-e.g. when specializing @f ~ ((->) e)@, then
-
-@evalSexp :: ([a] -> e -> a) -> ([a] -> g -> e -> a) -> Sexp g a -> (e -> a)@
-
-(evaluation just takes an environment, passing it down to the two evaluators.)
-
+i.e. strictly evaluate a sexp ("all the way") to an atom, within any monadic context.
 
 -}
 evalSexp :: (Monad m) => ([a] -> m a) -> ([a] -> g -> m a) -> Sexp g a -> m a
 evalSexp list apply = \case
- Atom a    -> return a
- List   ss -> list           =<< mapM (evalSexp list apply) ss
- Sexp g ss -> (flip apply) g =<< mapM (evalSexp list apply) ss
+ Atom a    -> pure a
+ List   es -> list           =<< traverse go es
+ Sexp g es -> (flip apply) g =<< traverse go es
+ where
+ go = evalSexp list apply
+
+{-| 
+
+>>> data ArithFunc = Add | Multiply | Negate deriving Show 
+>>> let badArith  = Sexp Negate [Atom 1, Atom 2, Atom 3] :: Sexp ArithFunc Integer 
+>>> let goodArith = Sexp Add [Sexp Multiply [], Sexp Negate [Atom (10::Integer)], Sexp Multiply [Atom 2, Atom 3, Atom 4]]
+>>> :set -XLambdaCase
+>>> :{
+let evalArith = \case
+                 Add      -> \case
+                              xs    -> Just [sum xs]
+                 Multiply -> \case
+                              xs    -> Just [product xs]
+                 Negate   -> \case
+                              [x]   -> Just [negate x] 
+                              _     -> Nothing 
+:}
+>>> evalSplatSexp (flip evalArith) (fmap (:[]) badArith)  -- wrong arity 
+Nothing 
+>>> evalSplatSexp (flip evalArith) (fmap (:[]) goodArith) -- (+ (*) (- 10) (* 2 3 4))
+Just [15] 
+
+specializing, as above, @(m ~ Maybe)@, @(b ~ [Integer])@, @(g ~ ArithFunc)@:
+
+@evalSplatSexp :: ([Integer] -> ArithFunc -> Maybe [Integer]) -> (Sexp ArithFunc [Integer] -> Maybe [Integer])@ 
+
+@evalSplatSexp apply = 'evalSexp' ('pure'.'fold') (apply.'fold')@ 
+
+-}
+evalSplatSexp :: (Monad m, Monoid b) => (b -> g -> m b) -> (Sexp g b -> m b)
+evalSplatSexp apply = evalSexp (pure.fold) (apply.fold)
+{-# INLINE evalSplatSexp #-}
 
 {-| 
 
@@ -190,22 +259,16 @@ when a Sexp\'s atoms are 'Monoid'al ("list-like"),
 after evaluating some expressions into atoms,
 we can "splat" them back together.
 
-@splatList@ takes: a monadic evaluator @eval@
-and a list of s-expressions @ss@ to evaluate in sequence.
+@splatList@ takes: 
+
+* an evaluator @eval@
+
+* and a list of s-expressions @es@ to evaluate in sequence.
 
 -}
-splatList :: (Applicative m, Monoid xs) => (Sexp g xs -> m xs) -> [Sexp g xs] -> m xs
-splatList eval ss = fold <$> traverse eval ss
-
-{-| 
-
-e.g. specialized: 
-
-@evalSplatSexp :: ([x] -> g -> Maybe [x]) -> (Sexp g [x] -> Maybe [x])@ 
-
--}
-evalSplatSexp :: (Monad m, Monoid xs) => (xs -> g -> m xs) -> (Sexp g xs -> m xs)
-evalSplatSexp apply = evalSexp (return.fold) (apply.fold)
+splatSexpList :: (Applicative m, Monoid b) => (Sexp g b -> m b) -> [Sexp g b] -> m b
+splatSexpList eval = fmap fold . traverse eval 
+{-# INLINE splatSexpList #-}
 
 {-| 
 
@@ -215,6 +278,7 @@ List [Atom 1,Atom 2,Atom 3]
 -}
 listSexp :: [a] -> Sexp f a
 listSexp = List . map Atom 
+{-# INLINE listSexp #-}
 
 -- data SexpF a r
 --  = AtomF a
